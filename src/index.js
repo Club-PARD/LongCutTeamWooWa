dc4d6cd74b3b6befcf1c9577fdfae36054b35dcf
J-shooni <jshooni9803@gmail.com>
2023-06-19 16:24:53 +0900

    ModaSheetHeader

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
  import ReactDOM from 'react-dom/client';
  import App from './App';
- import ModalSheet from'./presentation/components/ModalSheet';
+ import ModalSheet from'./presentation/components/ModalSheetHeader';
  import "./index.css";
 import TestPage from './Test';
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
-<<<<<<< HEAD
     //  <App />
     <ModalSheet/>
-=======
-    <TestPage/>
-    //  <App />
->>>>>>> 282236a85c2fb837b8bb61dd7a96cdabcfb310bd
  );
