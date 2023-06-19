d3d26b7291ffa51b95bb9d10a7f6c1501b04a641
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-06-19 15:34:52 +0900

    modal tag selection title added

--- src/presentation/components/modal/ModalTagSelection.js
+++ src/presentation/components/modal/ModalTagSelection.js
@@ -2,12 +2,17 @@ import ModalTag from "./ModalTag";
 
 import "./ModalStyle.css";
 
-const ModalTagSelection = ({modalTagList, width}) => {
+const ModalTagSelection = ({title, modalTagList, width}) => {
     return (
+        <div className="tag-chips-container">
+        <div className="chip-title">
+            {title}
+        </div>
         <div className="tag-chips" style={{width: `${width}px`}}>
             {modalTagList.map((tag, index) => (
                 <ModalTag text={tag}/>
             ))}
+        </div>
         </div>
     );
 }
