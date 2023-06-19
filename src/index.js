18178223395c7b4025f68ed31ed07539618641b5
ParkJiSung <gggg0195@gmail.com>
2023-06-20 00:05:41 +0900

    SubmitBtn, InputTitle, HomePage 수정

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ import TestPage from './Test';
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
 
-<<<<<<< HEAD
-    <App />
-   //  <ModalSheet/>
 
-=======
    <TestPage/>
->>>>>>> 28fd4b779e68556d13996236d62b7fa58404d0c0
- );
+);
