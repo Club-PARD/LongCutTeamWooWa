4ab02cb1f73e662d33198142c077f17c47c70d16
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-06-19 16:40:07 +0900

    debug

--- src/presentation/components/modal/ModalSheet.js
+++ src/presentation/components/modal/ModalSheet.js
@@ -1,5 +1,6 @@
 import Divider from "../Divider";
 import ModalTagSelection from "./ModalTagSelection";
+const tags = ['도전정신', '성취감', '동기부여', '학습', '자기존중', '문제 해결 능력', '협업', '리더십', '커뮤니케이션', '신체적', '정서적'];
 
 const ModalSheet = ({isTemplate}) => {
     return (
