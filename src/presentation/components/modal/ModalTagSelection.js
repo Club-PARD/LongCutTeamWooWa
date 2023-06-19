c4a630915b4f3f08f6e7a1ceb52c036ea099df64
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-06-19 15:56:51 +0900

    button added in the modal tag selection

--- src/presentation/components/modal/ModalTagSelection.js
+++ src/presentation/components/modal/ModalTagSelection.js
@@ -1,18 +1,22 @@
 import ModalTag from "./ModalTag";
+import { FiPlus } from 'react-icons/fi';
 
 import "./ModalStyle.css";
 
 const ModalTagSelection = ({title, modalTagList, width}) => {
     return (
         <div className="tag-chips-container">
-        <div className="chip-title">
-            {title}
-        </div>
-        <div className="tag-chips" style={{width: `${width}px`}}>
-            {modalTagList.map((tag, index) => (
-                <ModalTag text={tag}/>
-            ))}
-        </div>
+            <div className="chip-title">
+                {title}
+            </div>
+            <div className="tag-chips" style={{width: `${width}px`}}>
+                {modalTagList.map((tag, index) => (
+                    <ModalTag text={tag}/>
+                ))}
+                <button className="add-button">
+                    <FiPlus />
+                </button>
+            </div>
         </div>
     );
 }
