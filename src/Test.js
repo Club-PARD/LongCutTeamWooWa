f4ed7575f473cde7746327414662cc1645333407
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-06-20 14:35:58 +0900

    resolve

--- src/Test.js
+++ src/Test.js
@@ -1,18 +1,9 @@
 import ModalSheet from "./presentation/components/modal/ModalSheet";
 import React from "react";
-<<<<<<< HEAD
 import styled from "styled-components";
 // import PopUp from "./presentation/components/popup/PopUp";
 // import save_icon from "./assets/img/popup_save.svg";
 import DatePicker from "./presentation/components/DatePicker";
-
-const TestPage = () => {
-    return (
-        <DatePicker/>
-=======
-import PopUp from "./presentation/components/popup/PopUp";
-import Image from "./assets/img/popup_save.svg";
-import PopUpBuilder from "./presentation/components/popup/PopUpBuilder";
 
 const TestPage = () => {
     return (
