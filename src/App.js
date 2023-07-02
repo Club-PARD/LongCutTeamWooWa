5d4729976be03e5c3dc3b03bddecbb6182d01025
J-shooni <jshooni9803@gmail.com>
2023-07-02 19:00:29 +0900

    PR-ADD 폰트적용 확인해주세요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import HomePage from './presentation/pages/HomePage';
+import "./assets/Fonts/Font.css"; //폰트적용 
 
  function App() {
   return (
