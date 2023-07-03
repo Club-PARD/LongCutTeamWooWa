f31d4ebe2dab29788c0925d100472f9e358ca049
ParkJiSung <gggg0195@gmail.com>
2023-07-03 13:50:38 +0900

    update 이미지 추가 모달 수정

--- src/presentation/components/dragAndDrop/DragAndDrop.js
+++ src/presentation/components/dragAndDrop/DragAndDrop.js
@@ -1,8 +1,44 @@
-import { Upload, Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import useFilePreview from './hooks/useFilePreview';
+import { Upload, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import useFilePreview from "./hooks/useFilePreview";
+import styled from "styled-components";
 
 const { Dragger } = Upload;
+
+const Text = styled.p`
+  font-family: ${(props) => props.theme.fontFamily.mainfont};
+  font-weight: ${(props) => props.theme.fontWeights.semibold};
+  font-size: ${(props) => props.theme.fontSizes.Body1};
+  color: ${(props) => props.theme.color.blackHigh};
+  line-height: 22px;
+  text-align: center;
+`;
+
+const DraggerWrapper = styled(Dragger)`
+  .ant-upload {
+    width: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+`;
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CustomButton = styled(Button)`
+  font-family: ${(props) => props.theme.fontFamily.mainfont};
+  font-weight: ${(props) => props.theme.fontWeights.semibold};
+  font-size: ${(props) => props.theme.fontSizes.Body2};
+  flex: column;
+  text-align: center;
+  border-radius: 42.481px;
+  background: var(--black-high, #272727);
+  color: var(--surface-white, #fff);
+  line-height: 160%;
+  margin-top: 15px;
+`;
 
 const DragAndDrop = ({ addFile, removeFile, handleSubmit }) => {
   const [handlePreview, previewContent] = useFilePreview();
@@ -14,7 +50,7 @@ const DragAndDrop = ({ addFile, removeFile, handleSubmit }) => {
 
   return (
     <>
-      <Dragger
+      <DraggerWrapper
         multiple={true}
         onRemove={removeFile}
         showUploadList={true}
@@ -26,14 +62,19 @@ const DragAndDrop = ({ addFile, removeFile, handleSubmit }) => {
         <p className="ant-upload-drag-icon">
           <PlusOutlined />
         </p>
-        <p className="ant-upload-text">
-          Click this area or drag files to upload
-        </p>
-      </Dragger>
+        <Text>
+          클릭하거나 그래그하여
+          <br />
+          대표 이미지를 업로드하세요
+        </Text>
+      </DraggerWrapper>
+
       {previewContent}
-      <Button type="primary" onClick={handleSubmit}>
-        Submit
-      </Button>
+      <CenteredContainer>
+        <CustomButton type="primary" onClick={handleSubmit}>
+          업로드
+        </CustomButton>
+      </CenteredContainer>
     </>
   );
 };
