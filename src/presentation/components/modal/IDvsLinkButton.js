e56feb333075cf95b0407332e000300091d1eecb
ParkJiSung <gggg0195@gmail.com>
2023-07-05 17:30:20 +0900

    update 링크 추가하기 효과적용

--- src/presentation/components/modal/IDvsLinkButton.js
+++ src/presentation/components/modal/IDvsLinkButton.js
@@ -1,7 +1,73 @@
-import styled from "styled-components";
-import React, { useState } from "react";
-import { keyframes } from "styled-components";
+// import styled from "styled-components";
+// import React, { useState } from "react";
+// import { keyframes } from "styled-components";
 
+
+// const ButtonContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   width: 330px;
+//   height: 41px;
+//   flex-shrink: 0;
+//   border-radius: 36px;
+//   background: var(--serch-background, #EFEFEF);
+//   padding : 0 4px; 
+//   padding-right : 4px;
+//     margin-top : 0px;
+// `;
+
+// const Button = styled.button`
+//   width: 162px;
+//   height: 33px;
+//   flex-shrink: 0;
+//   border-radius: 34.333px;
+//   background: ${(props) =>
+//     props.isActive ? "var(--surface-white, #FFF)" : "transparent"};
+//   font-size: ${(props) => (props.isActive ? "14px" : "14px")};
+//   font-family: Pretendard;
+//   font-style: normal;
+//   font-weight: ${(props) => (props.isActive ? "600" : "400")};
+//   line-height: 160%;
+//   text-align: center;
+//   border: none;
+//   outline: none;
+//   cursor: pointer;
+
+// `;
+
+// function IDvsLinkButton({isActive, handleClick}) {
+//   return (
+//     <ButtonContainer>
+//       <Button
+//         isActive={isActive}
+//         onClick={() => handleClick(false)}
+//       >
+//         ID로 불러오기
+//       </Button>
+//       <Button
+//         isActive={!isActive}
+//         onClick={() => handleClick(true)}
+//       >
+//         개별 링크로 불러오기
+//       </Button>
+//     </ButtonContainer>
+//   );
+// }
+
+// export default IDvsLinkButton;
+
+import styled, { keyframes } from "styled-components";
+import React, { useState } from "react";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -12,9 +78,9 @@ const ButtonContainer = styled.div`
   flex-shrink: 0;
   border-radius: 36px;
   background: var(--serch-background, #EFEFEF);
-  padding : 0 4px; 
-  padding-right : 4px;
-    margin-top : 0px;
+  padding: 0 4px; 
+  padding-right: 4px;
+  margin-top: 0px;
 `;
 
 const Button = styled.button`
@@ -33,10 +99,11 @@ const Button = styled.button`
   border: none;
   outline: none;
   cursor: pointer;
-
+  opacity: ${(props) => (props.isActive ? "1" : "0.4")};
+  transition: opacity 1.5s ease-in-out;
 `;
 
-function IDvsLinkButton({isActive, handleClick}) {
+function IDvsLinkButton({ isActive, handleClick }) {
   return (
     <ButtonContainer>
       <Button
